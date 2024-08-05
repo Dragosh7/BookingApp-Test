5cc94e11135c3e50e9ea7aa392c00bcaacb62bc3
Dragosh7 <dragos.tecuci.ext@siemens.com>
2024-08-05 12:58:23 +0300

    Explore page tests

---
+++ src/test/java/Explore.java
@@ -0,0 +1,59 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+//--
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import org.openqa.selenium.By;
+//--
+import org.openqa.selenium.WebDriver;
+//--
+import org.openqa.selenium.WebElement;
+//--
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+//--
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+//--
+import org.testng.annotations.AfterClass;
+//--
+import org.testng.annotations.BeforeClass;
+//--
+import org.testng.annotations.Test;
+
+
+public class Explore {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+        // options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void afterClass() throws InterruptedException{
+
+        Thread.sleep(1000);
+        driver.quit();
+
+    }
+
+    @Test
+    public void SomethingToTest() {
+
+    }
+
+
+
+}
